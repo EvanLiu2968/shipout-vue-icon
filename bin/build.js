6c12ae4c1667bf1a34592fa7a360d0d688f6c956
Cory <coryisbest0728@gmail.com>
2021-10-21 10:38:37 -0700

    Update build.js

--- bin/build.js
+++ bin/build.js
@@ -46,8 +46,7 @@ const generateIconCode = async (icon) => {
   const code = fs.readFileSync(location)
   const svgCode = await processSvg(code)
   const ComponentName = names.componentName
-  console.log(JSON.stringify(names, null, 2));
-  console.log(JSON.stringify(getAttrs(names.style, icon), null, 2));
+  console.log(JSON.stringify(icon, null, 2));
   const component = getElementCode(ComponentName, attrsToString(getAttrs(names.style, icon), names.style), icon, svgCode)
 
   fs.writeFileSync(destination, component, 'utf-8');
