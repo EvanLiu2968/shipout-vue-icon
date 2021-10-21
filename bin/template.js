06282b8afec03ee03408df609c36e472785d63b9
Cory <coryisbest0728@gmail.com>
2021-10-21 00:09:05 -0700

    Update template.js

--- bin/template.js
+++ bin/template.js
@@ -1,8 +1,8 @@
 const getAttrs = (style, iconData) => {
   const baseAttrs = {
     'xmlns': 'http://www.w3.org/2000/svg',
-    ':width': 'size',
-    ':height': 'size',
+    'width': iconData.width,
+    'height': iconData.height,
     'viewBox': `0 0 ${iconData.width} ${iconData.height}`,
     'aria-hidden': 'true',
     'v-on': '$listeners'
@@ -24,6 +24,8 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
   <template>
     <svg
       ${attrs}
+      :width="size || width"
+      :height="size || height"
     >
       ${svgCode}
     </svg>
@@ -39,6 +41,12 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
           type: String,
           default: "currentColor"
         }
+      },
+      data(): {
+        return {
+          width: attrs.width,
+          height: attrs.height
+        };
       }
     };
   </script>
